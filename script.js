f82a71eb22639132724bd129976263cf5cedbc37
Ny Andry <155851768+NY-ANDRY@users.noreply.github.com>
2025-12-23 23:58:33 +0300

    Refactor FileViewer initialization in script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ import MapBuilder from './class/MapBuilder.min.js';
 import FileViewer from './class/FileViewer.min.js';
 import FileListner from './class/FileListner.min.js';
 
-const reader = new FileViewer("file-reader-space", "file-reader", "close-file-reader");
 const map = new MapBuilder("map.php", "folders", "back", "loading");
+const reader = new FileViewer("file-reader-space", "file-reader", "close-file-reader");
 const fileListener = new FileListner("files.php", "list", "file-info-space");
 
 map.addReader(reader);
@@ -11,5 +11,6 @@ map.addListner(fileListener);
 map.rename("LOCALHOST");
 map.setFileIcon("assets/file.svg");
 map.setFolderIcon("assets/folder2.svg");
+fileListener.setPathArgsName("path");
 
 map.build();
